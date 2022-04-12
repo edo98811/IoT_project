efa412917a521c016b6646901d6c7fc006d74e10
Edoardo Filippi <edoardofilippi77@gmail.com>
2022-04-12 13:57:41 +0000

    Upload New File

---
+++ server_mount.py
@@ -0,0 +1,17 @@
+import cherrypy
+from catalog_manager import catalog
+from location_service import location_service
+
+if __name__ == '__main__':
+    conf = {
+        '/': {
+                'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+                'tool.session.on': True
+        }
+    }
+    cherrypy.tree.mount(location_service(), '/location service', conf)
+    cherrypy.tree.mount(catalog(), '/catalog_manager', conf)
+    # this is needed if you want to have the custom error page
+    # cherrypy.config.update({'error_page.400': error_page_400})
+    cherrypy.engine.start()
+    cherrypy.engine.block()
