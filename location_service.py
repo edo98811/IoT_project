57da47600db7e8bf79f81c383af9fd69d94c9ee5
Edoardo Filippi <edoardofilippi77@gmail.com>
2022-04-20 16:15:01 +0000

    Replace location_service.py

--- location_service.py
+++ location_service.py
@@ -1,51 +1,70 @@
 
 from math import dist
+import json
+import time
 import requests
+import cherrypy
 
 
 class location_service():
-    def __init__(self,catalog,):
-        self.catalog = catalog
-        self.clinics_pos = requests.get(catalog)
-        self.patient_list = requests.get(catalog)
+    exposed=True
+    def __init__(self,catalog_address):
+        self.catalog_address = catalog_address
+        self.clinics = []
+        self.patient_list = []
 
-        self.nearest = []
 
-        for patient in self.patient_list 
-
-            self.nearest.append({
-                'patient_ID' :'patient_id',
-                'clinic_pos':'',
-                'patient_pos':'',
-                'nearest':'',
-            })
+    def GET(self, *uri, **params): #da riguardare come funzionano bene i rest
+        p_ID = params["p_ID"]#['patient_id']
             
-    
+        patient_info = next((p for p in self.nearest if p['d_ID'] == p_ID), None) #estrae il dizionario relativo al paziente e lo restituisce alla clinica che dovrà fare un controllo sulla presenza o meno dei campi 
 
-    def GET(self,msg):
-        pat_id = msg['patient_id']
+        return patient_info
+        
+    @cherrypy.tools.json_in()
+    def POST(self, *uri, **payload): #è giusto in questo modo?
+        msg=cherrypy.request.json
+        if not self.clinics:
+            #posso mandare richieste al server stesso secondo google quindi dovrebbe essere ok
+            self.clinics = json.loads(requests.get(self.catalog_address + '/get_clinics').text) #mettere in modo che i formati che escano dalle richieste siano corretti 
+            self.patient_list = json.loads(requests.get(self.catalog_address + '/get_patients').text)
 
-        #trova la clinic più vicina
+            self.nearest = []
 
-        return nearest_clinic
+            for patient in self.patient_list: 
 
+                self.nearest.append({
+                    'patient_ID':patient['patient_ID'],
+                    'clinic_pos':'',
+                    'patient_pos':'',
+                    'nearest':'',
+                    'clinic_address':'', #mandato questo json (che viene mandato così perchè viene estratto da questa lista)
+                })
 
+        #msg = json.loads(payload)
 
-    def POST(self,msg): #vedere come si fanno e richieste in rest e se si usa il topic cosi 
-        pat_pos = msg['pos']
+        pat_pos = msg['location']
+        p_ID = msg['p_ID']
 
-        for clinic,i in enumerate(self.clinics_pos):
+        for clinic,i in enumerate(self.clinics):
+
             cl_pos = clinic['clinic_pos']
 
             #calcolo della piu vicina
-            if dist < dist_temp:
-                dist_temp = dist
-                nearest_id = i
+            d = dist([pat_pos['longitude'],pat_pos['latitude']],[cl_pos["longitude"],cl_pos["latitude"]])
+            if d < dist_temp:
+                dist_temp = d
+                nearest_index = i #prende l'indice della clinica più viina la paziente
 
-        #p_index = filter(lambda patient: self.patient_list['patient_ID'] == msg['pat_id'], people) #come posso ricavare il pateinteindex?
-        self.nearest[p_index]['nearest'] == self.clinics_pos[nearest_id]['clinic_id']
+        for patient, i in enumerate(self.nearest): #cerca l'indice del paziene nel json con le posizione per poterla aggiornare
+            if patient['patient_ID'] == p_ID:
+                p_index = i
+            
+
+        #aggiorna la posizione 
+        self.nearest[p_index]['nearest'] == self.clinics_pos[nearest_index]['clinic_id']
         self.nearest[p_index]['patient_pos'] == msg['pos']
-        self.nearest[p_index]['clinic_pos'] == self.clinics_pos[nearest_id]['clinic_pos']
+        self.nearest[p_index]['clinic_pos'] == self.clinics_pos[nearest_index]['clinic_pos']
 
     #def update_pos(self): forse non serve
 
