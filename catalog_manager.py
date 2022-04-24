564736c12b8a6207885a51e35d97675cd5f66b63
Davide D'Adamo <davide.dadamo.98@gmail.com>
2022-04-24 18:16:58 +0000

    POST nuovo paziente

--- catalog_manager.py
+++ catalog_manager.py
@@ -75,89 +75,91 @@ class catalog():
             doctor = next((d for d in catalog['doctors'] if d['doctor_ID'] == d_ID), None) # questa funzione crea un iterator della lista (un oggetto sostanzialmente che applica una funzione a qualche altro oggetto, in questo caso applica un controllo ad una lista)
 
             return json.dumps(doctor['address']) # se serve un campo in particolare
-        
-        else: 
-            #cherrypyerror
-            pass
+	
+	else: 
+		#cherrypyerror
+		pass
 
-def POST(self,*uri,**params):
+	def POST(self,*uri,**params):
+	
+	# Estrae il catalog dal file
+	f=open(self.catalog_file,'r')
+	catalog = json.load(f)
+	f.close		
+
+	if uri[0] == "p-rec":			#### ADD PATIENT ####	
+									# 	uri: /p-rec
+									# 	body del post:
+									# 		{
+									# 			name: ,
+									# 			surname: ,
+									# 			chatID: ,
+									# 			docName: ,
+									# 			docSurname: ,
+									# 			dev: 
+									# 		})		
 		
-		# Estrae il catalog dal file
-		f=open(self.catalog_file,'r')
-		catalog = json.load(f)
-		f.close		
+		pats=catalog["patients"]
+		docs=catalog["doctors"]
+		sens=catalog["sensors"]
 
-		if uri[0] == "p-rec":			#### ADD PATIENT ####	
-										# 	uri: /p-rec
-										# 	body del post:
-										# 		{
-										# 			name: ,
-										# 			surname: ,
-										# 			chatID: ,
-										# 			docName: ,
-										# 			docSurname: ,
-										# 			dev: 
-										# 		})		
-			
-			pats=catalog["patients"]
-			docs=catalog["doctors"]
-			sens=catalog["sensors"]
+		# Legge il body del POST richiesto da 'patient-rec.html' e lo visualizza nel terminal
+		newPat=json.loads(cherrypy.request.body.read())
+		pprint(newPat)
 
-			# Legge il body del POST richiesto da 'patient-rec.html' e lo visualizza nel terminal
-			newPat=json.loads(cherrypy.request.body.read())
-			pprint(newPat)
+		# Se il paziente è gia registrato mostra un banner di errore e indica di compiere il log-in
+		for p in pats:
+			if (p["personal_info"]["nome"]+p["personal_info"]["cognome"]).lower == (newPat["name"]+newPat["surname"]).lower:
+				return f"Hi {newPat['name']}, you are already registered!\nTo add a new device, please log in and follow the procedure"
+		
+		# Individua l'ID del medico curante del nuovo paziente
+		for d in docs:
+			if (d["name"]+d["surname"]).lower == (newPat["docName"]+newPat["docSurname"]).lower:
+				docID = d["doctor_ID"]
+			else:
+				docID=f"d_{len(docs)+1}"
+		
 
-			# Se il paziente è gia registrato mostra un banner di errore e indica di compiere il log-in
-			for p in pats:
-				if (p["personal_info"]["nome"]+p["personal_info"]["cognome"]).lower == (newPat["name"]+newPat["surname"]).lower:
-					return f"Hi {newPat['name']}, you are already registered!\nTo add a new device, please log in and follow the procedure"
-			
-			# Individua l'ID del medico curante del nuovo paziente
-			for d in docs:
-				if (d["name"]+d["surname"]).lower == (newPat["docName"]+newPat["docSurname"]).lower:
-					docID = d["doctor_ID"]
-				else:
-					docID=f"d_{len(docs)+1}"
-			
+		# Individua l'ID dei sensori posseduti dal paziente tra quelli presenti nel catalog
+		devID=[]
+		for d_newPat in newPat["dev"]:
+			for s in sens:					
+				if s["sensor_type"] == d_newPat:
+					devID.append(s["sensor_ID"])
 
-			# Individua l'ID dei sensori posseduti dal paziente tra quelli presenti nel catalog
-			devID=[]
-			for d_newPat in newPat["dev"]:
-				for s in sens:					
-					if s["sensor_type"] == d_newPat:
-						devID.append(s["sensor_ID"])
-
-			# Definisce la nuova scheda paziente e la inserisce nella variabile locale che rappresenta il catalog
-			f_newPat={
-				"patient_ID": f"p_{len(pats)+1}",
-				"personal_info": {
-					"nome": newPat["name"],
-					"cognome": newPat["surname"],
-					"malattia": ""
-				},
-				"sensors": devID,
-				"doctor_ID": docID,
-				"device_connector": {
-					"broker": "",
-					"port": "",
-					"id": "",
-					"topic":""
-				}
+		# Definisce la nuova scheda paziente e la inserisce nella variabile locale che rappresenta il catalog
+		f_newPat={
+			"patient_ID": f"p_{len(pats)+1}",
+			"personal_info": {
+				"nome": newPat["name"],
+				"cognome": newPat["surname"],
+				"malattia": ""
+			},
+			"sensors": devID,
+			"doctor_ID": docID,
+			"device_connector": {
+				"broker": "",
+				"port": "",
+				"id": "",
+				"topic":""
 			}
+		}
 
-			catalog["patients"].append(f_newPat)
+		catalog["patients"].append(f_newPat)
 
-			# Aggiorna 'catalog.json'
-			f=open(self.catalog_file,'w')
-			json.dump(catalog,f,indent=4)
-			f.close()
+		# Aggiorna 'catalog.json'
+		f=open(self.catalog_file,'w')
+		json.dump(catalog,f,indent=4)
+		f.close()
 
-			return f"Registration succeeded!\nWelcome {newPat['name']}"
+		return f"Registration succeeded!\nWelcome {newPat['name']}"
 
-        pass
-        #addsensor
-        #addpatient
-        #adddoctor
-        #addclinic
 
-    #deve rispondere a: location, data analysis,alert e altre? serve importare le librerie nelle classi ? 
+
+	pass
+	#addsensor
+	#addpatient
+	#adddoctor
+	#addclinic
+
+#deve rispondere a: location, data analysis,alert e altre? serve importare le librerie nelle classi ? 
