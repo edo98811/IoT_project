601cecbfa34dcdcc3268a27efd11d6a63799eafb
Edoardo Filippi <edoardofilippi77@gmail.com>
2022-04-26 11:18:02 +0000

    versione mia di MQTT, non cambia molto dalla sua

--- MQTT.py
+++ MQTT.py
@@ -1,17 +1,18 @@
 import json
-import paho.mqtt.client as mqtt_client
+import paho.mqtt.client as  mqtt_client
 
 class MQTT():
-    def __init__(self, broker, port, name, notifier):
+    def __init__(self,name, broker, port, notifier):
         self.name = name
         self.broker = broker
-        self.port = port
+        self.port = int(port)
         self.mqtt_service = mqtt_client.Client(name,True)
         self.notifier = notifier
+        self.topic = ""
 
         self._is_subscriber = True
-        self._paho_mqtt.on_connect = self.my_on_connect
-        self._paho_mqtt.on_message = self.my_on_message
+        self.mqtt_service.on_connect = self.my_on_connect
+        self.mqtt_service.on_message = self.my_on_message
 
     def my_on_connect (self, paho_mqtt, userdata, flags, rc):
         print ("Connected to %s with result code: %d" % (self.broker, rc))
@@ -20,25 +21,28 @@ class MQTT():
         self.notifier.notify (msg.topic, msg.payload)
  
     def my_publish (self, topic, msg):
-        self._paho_mqtt.publish(topic, json.dumps(msg), 2)
+        self.mqtt_service.publish(topic, json.dumps(msg), 2)
+        print ("message to: %s" % (topic))
 
     def my_subscribe (self, topic):
-        self._paho_mqtt.subscribe(topic, 2) 
+        self.mqtt_service.subscribe(topic, 2) 
         self._is_subscriber = True
         self.topic = topic
         print ("subscribed to %s" % (topic))
  
     def start(self):
-        self._paho_mqtt.connect(self.broker , self.port)
-        self._paho_mqtt.loop_start()
+        self.mqtt_service.connect(self.broker , self.port)
+        self.mqtt_service.loop_start()
 
     def unsubscribe(self):
         if (self._is_subscriber):
-            self._paho_mqtt.unsubscribe(self._topic)
+            self.mqtt_service.unsubscribe(self._topic)
+            print ("unsubscribed")
             
     def stop (self):
         if (self._is_subscriber):
-            self._paho_mqtt.unsubscribe(self._topic)
+            self.mqtt_service.unsubscribe(self._topic)
+            print ("unsubscribed")
  
-        self._paho_mqtt.loop_stop()
-        self._paho_mqtt.disconnect()
+        self.mqtt_service.loop_stop()
+        self.mqtt_service.disconnect()
