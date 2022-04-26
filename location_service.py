8b33fe39c3034786d62cb43f2e50b4deafdb6210
Edoardo Filippi <edoardofilippi77@gmail.com>
2022-04-26 11:16:41 +0000

    versione un po' più aggiornata

--- location_service.py
+++ location_service.py
@@ -14,19 +14,19 @@ class location_service():
         self.patient_list = []
 
 
-    def GET(self, *uri, **params): #da riguardare come funzionano bene i rest
-        p_ID = params["p_ID"]#['patient_id']
+    def GET(self, *uri, **params): 
+        p_ID = params["p_ID"]
             
-        patient_info = next((p for p in self.nearest if p['d_ID'] == p_ID), None) #estrae il dizionario relativo al paziente e lo restituisce alla clinica che dovrà fare un controllo sulla presenza o meno dei campi 
+        patient_info = next((p for p in self.nearest if p['patient_ID'] == p_ID), None) #estrae il dizionario relativo al paziente e lo restituisce alla clinica che dovrà fare un controllo sulla presenza o meno dei campi 
 
         return patient_info
-        
+
     @cherrypy.tools.json_in()
-    def POST(self, *uri, **payload): #è giusto in questo modo?
+    def POST(self, *uri, **payload):
         msg=cherrypy.request.json
         if not self.clinics:
             #posso mandare richieste al server stesso secondo google quindi dovrebbe essere ok
-            self.clinics = json.loads(requests.get(self.catalog_address + '/get_clinics').text) #mettere in modo che i formati che escano dalle richieste siano corretti 
+            self.clinics = json.loads(requests.get(self.catalog_address + '/get_clinics').text)
             self.patient_list = json.loads(requests.get(self.catalog_address + '/get_patients').text)
 
             self.nearest = []
@@ -38,7 +38,7 @@ class location_service():
                     'clinic_pos':'',
                     'patient_pos':'',
                     'nearest':'',
-                    'clinic_address':'', #mandato questo json (che viene mandato così perchè viene estratto da questa lista)
+                    'clinic_address':"" #mandato questo json (che viene mandato così perchè viene estratto da questa lista)
                 })
 
         #msg = json.loads(payload)
@@ -46,7 +46,7 @@ class location_service():
         pat_pos = msg['location']
         p_ID = msg['p_ID']
 
-        for clinic,i in enumerate(self.clinics):
+        for clinic,i in enumerate(self.clinics): # int objects is not subscriptable
 
             cl_pos = clinic['clinic_pos']
 
@@ -65,7 +65,7 @@ class location_service():
         self.nearest[p_index]['nearest'] == self.clinics_pos[nearest_index]['clinic_id']
         self.nearest[p_index]['patient_pos'] == msg['pos']
         self.nearest[p_index]['clinic_pos'] == self.clinics_pos[nearest_index]['clinic_pos']
-
+        self.nearest[p_index]['clinic_address'] = 'alert/clinica1'
     #def update_pos(self): forse non serve
 
 
