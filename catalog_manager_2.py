1b38828b4d8fe7c12ff2a527d07016dc506cc046
Edoardo Filippi <edoardofilippi77@gmail.com>
2022-04-29 11:24:22 +0000

    catalog manager senza POST commentato

--- catalog_manager_2.py
+++ catalog_manager_2.py
@@ -1,86 +1,120 @@
 import json
 #import requests
-#import cherrypy
-
-# va aggiunto un controllo params in tutte le funzioni del codice
-# nell'uri metto solo il tipo di richiesta (quindi da chi iene) ma devo tenere conto che c'è anche la prima parte dell'indirizzo 
-# importante far quadrare tutti i nomi di tutti 
+import cherrypy
 
 class catalog():
     exposed=True
     def __init__(self,catalog_file):
         self.catalog_file = catalog_file
 
-    def GET(self,*uri,**params): # come funzionano questi parametri?
+    def GET(self,*uri,**params): 
+        # legge il catalog dal file json e lo carica in una variabile temporanea catalog 
+        catalog = json.load(open(self.catalog_file,'r'))
 
-        # device connector info per avviarsi 
+        # device connector info
         if uri[0] == 'get_dc_info':
 
-            catalog = json.load(open(self.catalog_file,'r'))
-            p_ID = params['p_ID'] #cercare le info di un paziente conoscendo il suo ID, aggiungere try except magari 
-            # vediamo se funziona
-            pat = next((p for p in catalog['patients'] if p['patient_ID'] == p_ID ), None) # questa funzione crea un iterator della lista (un oggetto sostanzialmente che applica una funzione a qualche altro oggetto, in questo caso applica un controllo ad una lista)
+            # richiamato da device connector      
 
-            # msg = {
-            #     "broker":pat["device_connector"]["broker"],
-            #     "port":pat["device_connector"]["port"],
-            #     "topic":pat["device_connector"]["topic"],
-            # }
-            return json.dumps(pat['device_connector'])
+              
+            p_ID = params['p_ID']
 
-        #sensori elativi ad un paziente
+            # questa funzione crea un iterator della lista e mi restituisce le informazioni 
+            # sul paziente identificato dal p_ID passato alla funzione
+            # spiegato male: un iterator è un oggetto che applica una funzione ad un altro oggetto 
+            pat = next((p for p in catalog['patients'] if p['patient_ID'] == p_ID ), None) 
+
+            msg = {
+                "broker":pat["device_connector"]["topic"],
+                "port":pat["device_connector"]["port"],
+                "topic":catalog["MQTT_broker"],
+            }
+
+            return json.dumps(msg)
+
+        #sensori relativi ad un paziente
         elif uri[0] == 'get_sensors':
 
-            catalog = json.load(open(self.catalog_file,'r'))
-
+            # richiamato da device connector
 
             p_ID = params['p_ID']
           
+            # prende le info del paziente a recupera la lista dei sensori
             pat = next((p for p in catalog['patients'] if p['patient_ID'] == p_ID ), None)
             sensor_ids = pat["sensors"]
-            sensor_list = []
+            sensors = []
 
-            for sensor_id in sensor_ids:
+            # itera lungo la lista dei sensori e prende le loro informazioni dal catalog
+            for s_info in sensor_ids:
 
-                   sensor_list.append(next((s for s in catalog['sensors'] if s['sensor_ID'] == sensor_id ), None))
-                # va ok perchè in ogni caso ce ne dobbe essere solo uno di sensore, quindi l'output è un elemento solo e lo aggiunge alla lista
+                
+                # recupera le carattersitiche dei sensori
+                s_type = next((s for s in catalog['sensors'] if s['type_ID'] == s_info['type'] ), None)
 
-            sensor_list ={
-                'sl':sensor_list
+                # questo è il messaggio passato al sensor info del device connector
+                s_msg = {
+                    "is_critical":s_info["is_critical"],
+                    "safe_range":s_info["safe_range"],
+                    "type_ID":s_type["type_ID"],
+                    "type":s_type["type"],
+                    "range":s_type["range"],
+                    "unit":s_type["unit"]
+                }
+
+                # lo aggiunge ad una lista
+                sensors.append(s_msg)
+
+            # dato che la lista non si può mandare in un json creo un messaggio con una chiave associata alla lista
+
+            msg ={
+                'sl':sensors
             } 
-            return json.dumps(sensor_list)
+            return json.dumps(sensors)
 
 
-        #funzione per le cliniche
+        #per le cliniche
         elif uri[0] == 'get_clinics':
-
-            catalog = json.load(open(self.catalog_file,'r'))
-
+            # richiamata da alert_service
+        
             clinics = catalog["clinics"]
 
             return json.dumps(clinics)
 
-        #per tutti i pazienti 
-        elif uri[0] == 'get_patients':
-            catalog = json.load(open(self.catalog_file,'r'))
+        #per le informazioni di un paziente
 
-            return json.dumps(catalog['patients'])
+            # richiamato da: alert_service
 
-        #per il dottore di un paziente
+        elif uri[0] == 'get_patient_info':
+            p_ID = params["p_ID"]
+
+            # richiamato da alert service
+
+            patient = next((p for p in catalog['patients'] if p['patient_ID'] == p_ID), None)  
+            return json.dumps(patient)
+
+        # #per tutti i pazienti 
+        # elif uri[0] == 'get_patients':
+        #     # richiamato da
+
+        #     return json.dumps(catalog['patients'])
+
+        #per il dottore associato ad un paziente
         elif uri[0] == 'get_doctor':
-
-            catalog = json.load(open(self.catalog_file,'r'))
-
+            
+            # prima trova il paziente per cui devo ricercare il medico
             p_ID = params["p_ID"] 
             patient = next((p for p in catalog['patients'] if p['patient_ID'] == p_ID), None) 
-            d_ID = patient["doctor_ID"]
-            doctor = next((d for d in catalog['doctors'] if d['doctor_ID'] == d_ID), None)
-            # questa funzione crea un iterator della lista (un oggetto che applica una funzione a qualche altro oggetto, in questo caso applica un controllo ad una lista)
 
-            return json.dumps(doctor) # se serve un campo in particolare
+            # poi cerco le informazioni del medico salvate nella lista dei medici
+            d_ID = patient["doctor_ID"]
+            doctor = next((d for d in catalog['doctors'] if d['doctor_ID'] == d_ID), None) 
+
+            return json.dumps(doctor) 
         
         else: 
-            #cherrypyerror
+            raise cherrypy.HTTPError(
+                400,
+                "The request entity could not be decoded. The following command doesn't exist {uri[0]}")
             pass
 
     def POST():
