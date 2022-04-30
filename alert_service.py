c03fe26016e7f9283578f04978e8e7bcdfcc5925
Edoardo Filippi <edoardofilippi77@gmail.com>
2022-04-30 15:17:31 +0000

    commentato

--- alert_service.py
+++ alert_service.py
@@ -2,85 +2,163 @@
 import json
 import time
 
-from numpy import empty
 from MQTT import *
+from MyMQTT import *
 import requests as r
 
-from location_service import location_service
-
-# nota: magari si potrebbe anche mandare alla clinica il numero di telefono nel messaggio, così che possano verificare che non ci sia un errore nei sensori o qualcosa di simile 
-# e non mandino qualcuno o si attivino inutilmente
-
-# si potrebbe anche aggiungere un controllo su dati successivi, del tipo che se ce ne sono 5 fuori range di fila si attiva
     
 class alert_service:
     def __init__(self, broker, port, ID, topic,catalog_address,location_service):
-        self.alert_service = MQTT(ID, broker, port, self)
+        self.alert_service = MyMQTT(ID, broker, int(port), self)
         self.alert_service.start()
-        self.alert_service.my_subscribe(topic)
-        self.catalog_address = catalog_address # tutti gli indirizzi si potrebbero salvare nel catalog e prenderli all'inizio con una richiwsta e poi aggiungere un controllo negli errori nel caso 
+        self.alert_service.mySubscribe(topic)
+        self.catalog_address = catalog_address 
         self.location_service = location_service
 
-    def notify(self, topic, msg): # controllare come funziona MQTT
-
-        patient_ID = msg['p_id'] # ricontrollare come funzionano i messaggi e fare il punto su come strutturarli 
-        measures = msg['measures']
+    def notify(self, topic, msg): 
 
 
-        for measure in measures:    #controlla le misure, posso aggiungere tutte quelle che voglio 
-            if measure['n'] =='bpm':
-                if True:    # da aggiungere la condizione qui 
-                    # 
-                    problem = 'fuori range di questo'
-                    stato = self.alert(patient_ID,problem) # a questo punto chiamo la funzione alert (basta richiamarlo ogni volta)
-                    print(stato)
-                    
-            elif measure['n'] =='glucosimetro': 
-                pass # sarebbe come sopra
+        # messaggio ricevuto da device connector
+        # template messaggio: 
 
-            elif measure['n'] =='qualcos altro':
+                # message = {			
+                # 'p_ID':patient_ID,
+                # 't':basetime,
+                # 'e':[ misurazioni
+                #         {               
+                #         'n':sensor_ID,
+                #         'vs':sensor_type,
+                #         'v':'',
+                #         't':time,
+                #         'u':unit
+                #         'is_critical':
+                #         'saferange':
+                #         },
+                #         {               
+                #         'n':sensor_ID,
+                #         'vs':sensor_type,
+                #         'v':'',
+                #         't':time,
+                #         'u':unit,
+                #         'is_critical':
+                #         'saferange':
+                #         },
+                #     ]
+                #     'latitude':0,
+                #     'longitude':0
+                # }
+
+        # prende le informazioni necessarie 
+        patient_ID = msg['p_ID']
+        measures = msg['e']
+
+        # itera lungo le misurazioni dei singoli sensori e controlla la criticità associata ad essa, nel caso ci sia un problema richiama i metodi di notifica 
+        # i metodi per le procedure di allerta sono definiti sotto 
+        for measure in measures:
+            
+            if measure["is_critial"] == "not_critical":
                 pass
 
-    def alert(self,patient_ID,problem):
+            # se la misurazione è informativa (allerta solo al paziente)
+            if measure["is_critial"] == "informative":
 
-        doctor = r.get(self.catalog_address + 'get_doctor',data = {"p_ID":patient_ID})
 
-        # implementare una funzioe per mandare il messaggio al doctor
-        print("attenzioneeee" + patient_ID + " sta morendo")
+                #messaggio che viene mandato insieme alla notifica
+                problem = f'lettura di {measure["vs"]} di {measure["v"]} fuori dal range di sicurezza impostato da {measure["v"][0]} a {measure["v"][1]}'
+                self.informative_alert(patient_ID,problem)
 
-        
-        nearest_clinic = r.get(self.location_service, data = {"p_ID":patient_ID})
+            # se la misuzione è critica (allerta a clinica e medico)
+            if measure["is_critial"] == "critical":
+
+                #messaggio che viene mandato insieme alla notifica
+                problem = f'lettura di {measure["vs"]} di {measure["v"]} fuori dal range di sicurezza impostato da {measure["v"][0]} a {measure["v"][1]}'
+                self.critical_alert(patient_ID,problem) # a questo punto chiamo la funzione alert (basta richiamarlo ogni volta)
+            
+
+    # allerta critica (medico e clinica)
+    def critical_alert(self,patient_ID,problem):
+
+
+        # get al catalog per informazioni di contatto del medico 
+        doctor = json.loads(r.get(self.catalog_address + 'get_doctor',data = {"p_ID":patient_ID}).text)
+
+        #print("attenzioneeee" + patient_ID + " sta morendo")
+        # messaggio da mandare alla clinica e al medico
+        msg = {
+                "p_ID":patient_ID,
+                "latitude":nearest_clinic['patient_pos']['longitudine'],
+                "longitude":nearest_clinic['patient_pos']['latitudine'],
+                "problem":problem, # messaggio che verrà letto
+                "doctor_contact":doctor["chat_ID"]
+            }
+
+        # ricava il contatto (ha ricevuto tutte le info)
+        contact = doctor["chat_ID"]
+
+        # messaggio mandato al dottore
+        self.alert_service.myPublish('telepot/alert/message', msg)
+
+        # get al location service per informazioni di contatto della clinica
+        nearest_clinic = json.loads(r.get(self.location_service, data = {"p_ID":patient_ID}).text)
+            # template messaggio ricevuto:
+                    # msg = {
+                    #     'patient_ID':patient['patient_ID'],
+                    #     'clinic_pos':'',
+                    #     'patient_pos':'',
+                    #     'nearest':'',
+                    #     'clinic_address':"" 
+                    #     })
 
         topic = nearest_clinic["clinic_address"]
 
+        # nel caso in cui il campo nearest sia vuoto non entra in questo blocco e non manda il messaggio (non si conosce la posizione della clinica)
         if nearest_clinic['nearest']:    
-            
+          
+            # messaggio da mandare alla clinica e al medico
             msg = {
                 "p_ID":patient_ID,
-                "latitudine":nearest_clinic['patient_pos']['longitudine'],
-                "longitudide":nearest_clinic['patient_pos']['latitudine'],
-                "problema":problem,
-                "doctor contact":"" # da completare
+                "latitude":nearest_clinic['patient_pos']['longitudine'],
+                "longitude":nearest_clinic['patient_pos']['latitudine'],
+                "problem":problem, # messaggio che verrà letto 
+                "doctor_contact":doctor["chat_ID"],
+                "doctor_name":doctor["name"] # magari da correggere
             }
-            self.alert_service.my_publish(topic, msg)
-            return 'messaggio mandato correttamente'
+
+            # messaggio mandato alla clinica
+            self.alert_service.myPublish(topic, msg)
+            print ('messaggio mandato correttamente alla clinica')
         else: 
-            return 'errore, non si conosce la posizione del paziente' # in questo caso manda solo un messaggio la medico (non è)
+            print ('errore, non si conosce la posizione del paziente') # in questo caso manda solo un messaggio la medico (non è aggiornata la posizione del paziente)
+
+    # allerta informativa (paziente)
+    def informative_alert(self,patient_ID,problem):
+        # get al catalog per informazioni di contatto del paziente
+        patient = json.loads(r.get(self.catalog_address + 'get_patient',data = {"p_ID":patient_ID}).text)
+
+        contact = patient["chat_ID"]
+        # messaggio
+        msg = {
+            "message":problem
+        }
+
+        # messaggio mandato al paziente (da aggiornare)
+        self.alert_service.myPublish('telepot/alert/message', msg)
+
 
 
 
 if __name__ =='__main__':
-    dati = json.load(open('settings_as.json','r')) # idea -> settings contiene tutte le impostazioni per i microservices in un dizionario 
-    # le impostazioni si potrebber anche mettere nel catalog e prenderle con un get invece di caricarle con settings e mettere solo il catalog come service
+    # carica i i dati relativi al client MQTT e agli indirizzi del location service e del catalog manager
+    dati = json.load(open('settings_as.json','r')) 
     location = dati['location_address']
     catalog = dati['catalog_address']
     topic = dati['topic']
     broker = dati['broker']
     port = dati['port']
     service_ID = dati['service_ID']
+    # avvia il servizio (subscriber MQTT)
     service =  alert_service(broker, port, service_ID, topic,location, catalog)
 
     done = False
     while not done:
-        #mettere condizione di stop?
         time.sleep(1)
