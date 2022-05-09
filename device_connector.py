35dffebe6c5d1e9b79bcb5c02708332957af53de
edo98811 <edoardofilippi77@gmail.com>
2022-05-09 18:06:27 +0200

    da ricontrollare ancora

--- device_connector.py
+++ device_connector.py
@@ -1,39 +1,50 @@
+from email import message
 import random
 from MyMQTT import *
-from MQTT import * # da toglierne uno
+from MQTT import *
 import requests
 import json 
 import time
 
 
 
-class sensor_def():                                                     ### classe del sensore
-    def __init__(self, sensor_type, sensor_ID, range, unit ):
+class sensor_def():                                                     ### definisce un nuovo sensore
+    def __init__(self, sensor_type, sensor_ID, range, safe_range):
         self.sensor_type = sensor_type
         self.sensor_ID = sensor_ID
-        self.range = range # lista di due numeri
-        self.unit = unit
-
+        self.range = range
+        self.safe_range = safe_range
     # genera un numero casuale nel range impostato (self.range)
-    def get_reading(self):                                          
-        output = random.randint(self.range[0],self.range[1])
+    def get_reading_safe(self):                                          
+        output = random.randint(self.safe_range[0],self.safe_range[1])
         return output
 
+    ### serve a noi per simulare una lettura fuori range -> 
+            # funziona cosi
+            # range -> [min safe_min safe_max max]
+            # se voglio generarare un numero fuori dal range "sicuro" lo genero tra safe_max e max, quindi all'interno 
+            # dell'intervallo di funzionamento del sensore ma fuori dal range di sicurezza 
+
+    def get_reading_alert(self):                                       
+        output = random.randint(self.safe_range[1],self.range[1])
+        return output
+    
 
 
 class device_connector():                                                 #classe del device connector (publisher MQTT)
-    def __init__(self, broker, port, patient_ID, topic,catalog_address):
-
-        self.dc = MQTT(patient_ID, broker, port, self)
-        self.topic = topic
+    def __init__(self, broker, port, patient_ID, topic, catalog_address):
 
         # avvia la connessione MQTT
+        self.dc = MQTT(patient_ID, broker, port, self)
         self.dc.start()
+
+        self.topic = topic
+        
         self.basetime = time.time() 
 
-        # template messaggio publisher
+        # template messaggio pubblicato dal DC
         self._message = {			
-            'patient_ID':patient_ID,
+            'p_ID':patient_ID,
             't':self.basetime,
             'e':[],
             'latitude':0,
@@ -44,46 +55,42 @@ class device_connector():                                                 #class
         self._sensors = []
 
         #prende dal catalog i sensori assegnati a questo device connector 
-        sensors = json.loads(requests.get(catalog_address + '/get_sensors',params= {'patient_ID':patient_ID}).text)#chiede la lista dei sensori del patient id che gli passo  
+        sensors = json.loads(requests.get(catalog_address + '/get_sensors',params= {'p_ID':patient_ID}).text) #chiede la lista dei sensori del patient id che gli passo  
 
-        # riceve un messaggio di questo tipo:
-                    # sensors = {
-                    #    'sl': [
-                    #       {
-                    #     "is_critical":s_info["is_critical"],
-                    #     "safe_range":s_info["safe_range"],
-                    #     "type_ID":s_type["type_ID"],
-                    #     "type":s_type["type"],
-                    #     "range":s_type["range"],
-                    #     "unit":s_type["unit"]
-                    #       },
-                    # ...
-                    #    ]
-                    # }
+            # sensors = [{
+            #       "type": sensor["sensor_type"],
+            #       "ID": sensor["sensor_ID"]
+            #       "range"
+            #       "safe_range"        
+            #   },
+            #   ... ,
+            #   {
+            #       "type": sensor["sensor_type"],
+            #       "ID": sensor["sensor_ID"]
+            #   }
+            # ]
 
 
-        #itera nella lista sl del messaggio ricevuto
-        for sensor in sensors['sl']:
+        # itera nella lista sl del messaggio ricevuto
+        for sensor in sensors:
 
             # utilizzando la classe sensor_def definisce i sensori associati al device connector
-            sensor = sensor_def(sensor['sensor_type'],sensor['sensor_ID'],sensor['range'],sensor['unit'])
-            self._sensors.append(sensor)
+            s = sensor_def(sensor['type'],sensor['ID'])
+            self._sensors.append(s)
 
             # template messaggio del sensore        
             self._message['e'].append({                            
-                'n':sensor.sensor_ID,
-                'vs':sensor.sensor_type,
-                'v':'',
-                'u':sensor.unit,
-                'is_critical':sensor.is_critical, 
-                'safe_range':sensor.critical_range
+                'n':s.sensor_ID,
+                'vs':s.sensor_type,
+                'v':'',#value
+                'u':json.loads(requests.get(catalog_address+"/sensor-general-info", params={"type":s.sensor_type}).text)["unit"]
             })
         
+        ## DA CONTROLLARE
     # prende il valore del sensore e lo inserisce nel messaggio 
     def get_readings(self): 
 
         for n,sensor in enumerate(self._sensors):
-
             value = sensor.get_reading() #
             self.message = self._message # copia il template 
             self.message['e'][n]['v'] = value
@@ -96,7 +103,8 @@ class device_connector():                                                 #class
         self.message['latitude'] = self.message['latitude'] + random.randint(-10,+10) # ipotizzando che la posizione vari casualmente di questa quantità
         self.message['longitude'] = self.message['latitude'] + random.randint(-10,+10)
 
-    
+
+        ## DA CONTROLLARE
     #pubblica il messaggio scritto in get_readings
     def send(self):
 
@@ -104,56 +112,71 @@ class device_connector():                                                 #class
         del self.message # elimina il messaggio dopo averlo mandato
         # il messaggio dovrebbe essere ricevuto dal data analysis
 
-   # template messaggio: 
-                                # msg = {			
-                                # 'patient_ID':patient_ID,
-                                # 't':basetime,
-                                # 'e':[ 
-                                #       {               
-                                #         'n':sensor_ID,
-                                #         'vs':sensor_type,
-                                #         'v':'value',
-                                #         't':time,
-                                #         'u':unit
-                                #         },
-                                #         {               
-                                #         'n':sensor_ID,
-                                #         'vs':sensor_type,
-                                #         'v':'',
-                                #         't':time,
-                                #         'u':unit,
-                                #         },
-                                #   ]
-                                #   'location':{        #un problema concettuale è che avendo la posizione qui non serve forse chiamare il catalog service
-                                #       'latitude':0,
-                                #       'longitude':0
-                                #   } 
-                                # }
+    # template messaggio inviato: 
+                # message = {			
+                # 'p_ID':patient_ID,
+                # 't':basetime,
+                # 'e':[ misurazioni
+                #         {               
+                #         'n':sensor_ID,
+                #         'vs':sensor_type,
+                #         'v':'',
+                #         't':time,
+                #         'u':unit
+                #         'iscritical':
+                #         'saferange':
+                #         },
+                #         {               
+                #         'n':sensor_ID,
+                #         'vs':sensor_type,
+                #         'v':'',
+                #         't':time,
+                #         'u':unit,
+                #         'iscritical':
+                #         'saferange':
+                #         },
+                #     ]
+                #     'latitude':0,
+                #     'longitude':0
+                # }
 
 
 
 if __name__ == '__main__':
     
-    catalog_address = 'http://127.0.0.1:8080/catalog_manager'
+####       CODICE DI "DEBUG"                                                            # Per motivi di comodità di progettazione e debug, preleva l'indirizzo del 
+    with open("./catalog.json",'r') as f:                                               # catalog manager dal catalog stesso, in modo da poter avere le informazioni 
+        cat = json.load(f)                                                              # centralizzate, e in caso di necessità cambiando tale indirizzo nel catalog,
+    host = cat["base_host"]                                                             # tutti i codici si adattano al cambio
+    port = cat["base_port"]
+    catalog_address = "http://"+host+":"+port+cat["services"]["catalog_manager"]["address"]
+####
+
+    # Di default il DC sa a quale paziente è associato, dunzue il patient_ID è definito all'interno del suo codice
     patient_ID = 'p_1'
 
     # manda una richiesta al catalog per i dati della connessione MQTT del device connector
-    pat_info = json.loads(requests.get(catalog_address + '/get_dc_info' ,params= {"patient_ID":patient_ID}).text)
-    print(pat_info)
+    pat_info = json.loads(requests.get(catalog_address + '/get_dc_info' ,params= {"p_ID":patient_ID}).text)
+            # msg = {
+            #             "broker":catalog["services"]["MQTT"]["broker"],
+            #             "port":catalog["device_connector"]["port"],
+            #             "topic":pat["device_connector"]["topic"],
+            #         }
+    
+    # Definizione del DC_1
     device_connector1 = device_connector(pat_info["broker"], pat_info["port"], patient_ID, pat_info["topic"],catalog_address)
+    
+    # # per simulare un sistema più complesso viene inizializzato un secondo device connector che funzionerà in parallelo al primo 
+    # patient_ID = 'p_2'
+    # pat_info = json.loads(requests.get(catalog_address + '/get_dc_info' ,params = {"p_ID":patient_ID}).text)
+    # print(pat_info)
+    # device_connector2 = device_connector(pat_info["broker"], pat_info["port"], patient_ID, pat_info["topic"], catalog_address)
 
-    # per simulare un sistema più complesso viene inizializzato un secondo device connector che funzinerà in parallelo al primo 
-    patient_ID = 'p_2'
-    pat_info = json.loads(requests.get(catalog_address + '/get_dc_info' ,params = {"patient_ID":patient_ID}).text)
-    print(pat_info)
-    device_connector2 = device_connector(pat_info["broker"], pat_info["port"], patient_ID, pat_info["topic"], catalog_address)
-
-
-    while True:
-        time.sleep(10)
-        device_connector1.get_readings()
-        device_connector1.send()
-        device_connector2.get_readings()
-        device_connector2.send()
+    # while True:
+    #     time.sleep(10)
+    #     device_connector1.get_readings()
+    #     device_connector1.send()
+    #     device_connector2.get_readings()
+    #     device_connector2.send()
 
  
