7c903e4cd071d1d10c48f70ce3f7b68b7123b766
Davide D'Adamo <davide.dadamo.98@gmail.com>
2022-06-09 00:04:54 +0200

    Risolto problema personal
    Eliminato controllo sul timestamp dei messaggi di alert

--- Alert_Service/alert_service.py
+++ Alert_Service/alert_service.py
@@ -12,8 +12,7 @@ class alert_service:
         self.catalog_address = catalog_address 
         self.location_service = location_service
         self.baseTopic = json.loads(r.get(catalog_address +"/get_service_info", params = {'service_ID':'MQTT'}).text)["baseTopic"]
-        self.time_s = time.time()
-        self.tl = 10 # time limit 
+
 
     def notify(self, topic, msg): 
 
@@ -69,25 +68,26 @@ class alert_service:
                 pass
 
             elif is_critical["is_critical"] == "personal":
+                
                 part1 = f"Pay attention {patient_info['personal_info']['name']} {patient_info['personal_info']['surname']}!\n\
                 Your device ({sensor_info[n]['type']}) is recording a value outside of your safe range"
 
-                if float(measure['v']) > float(is_critical['safe_range'][1]) and time.time() - self.time_s > self.tl:
+                if float(measure['v']) > float(is_critical['safe_range'][1]):
                     part2 = f"({measure['v']} {sensor_info[n]['unit']} > {is_critical['safe_range'][1]} {sensor_info[n]['unit']})\n\
                         Please, follow this measure (suggested by your personal doctor):\n\
                                 {sensor_info[n]['over_safe']}"
                     self.personal_alert(patient_ID, f"{part1} {part2}")
-                    self.time_s = time.time()
+                   
 
-                elif float(measure['v']) < float(is_critical['safe_range'][0]) and time.time() - self.time_s > self.tl:
+                elif float(measure['v']) < float(is_critical['safe_range'][0]):
                     part2 = f"({measure['v']} {sensor_info[n]['unit']} < {is_critical['safe_range'][1]} {sensor_info[n]['unit']})\n\
                         Please, follow this measure (suggested by your personal doctor):\n\
                         {sensor_info[n]['under_safe']}"
                     self.personal_alert(patient_ID, f"{part1} {part2}")
-                    self.time_s = time.time()
+                   
 
             elif is_critical["is_critical"] == "critical":
-                if (float(measure['v']) > float(is_critical['safe_range'][1]) or float(measure['v']) < float(is_critical['safe_range'][0])) and time.time() - self.time_s > self.tl:
+                if (float(measure['v']) > float(is_critical['safe_range'][1]) or float(measure['v']) < float(is_critical['safe_range'][0])):
                     problem = f"Warning! Critical event ongoing for patient: {patient_info['personal_info']['name']} {patient_info['personal_info']['surname']}\n\
                         Recorded by device: {sensor_info[n]['type']}\n\
                         Value: {measure['v']} {sensor_info[n]['unit']}\n\
@@ -96,9 +96,10 @@ class alert_service:
                             \tlon = {msg['e'][1]['v']}\n\
                         "
                     self.critical_alert(patient_ID,problem) # a questo punto chiamo la funzione alert (basta richiamarlo ogni volta)
-                    self.time_s = time.time()
+                   
             else: 
                 pass
+        
                 
     # allerta critica (medico e clinica)
     def critical_alert(self,patient_ID,problem):
